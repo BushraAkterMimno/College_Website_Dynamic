20587cb7d89d8ce79ad86b2fe4a9d5466eb4e5ac
unknown <bushraaktermimno@gmail.com>
2025-01-18 23:35:03 +0600

    notice page added

--- notice.php
+++ notice.php
@@ -295,73 +295,7 @@
 
     <!-- menu start -->
 
-        <section class="header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-10">
-                        <div class="wow bounceInUp logo logo" data-wow-duration="2s">
-                            <a href="home.html"><img src="img/logo/menu-logo.png" height="70px" width="70px"></a>
-                        </div>
-                        <div class="name">
-                            <strong><p><a href="home.html">BHRS COLLEGE</a></p></strong>
-                        </div>
-                        <div id="nav">
-                            <ul>
-                                <li class="padding"><a href="home.html">Home</a></li>
-                                <li><a href="#">About Us &raquo;</a>
-                                    <ul>
-                                        <li><a href="about/breif-history.html">Brief History</a></li>
-                                        <li><a href="about/gallery.html">Gallery</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Accademic &raquo;</a>
-                                    <ul>
-                                        <li><a href="accademic/Degree (Pass).html">DEGREE (PASS)</a></li>
-                                        <li><a href="accademic/Honours.html">HONORS</a></li>
-                                        <li><a href="accademic/master.html">MASTER'S</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Co-Curricolam &raquo;</a>
-                                    <ul>
-                                        <li><a href="co-curricolam/hostel info.html">Hostel Information</a></li>
-                                        <li><a href="co-curricolam/librery info.html">Library Information</a></li>
-                                        <li><a href="co-curricolam/bncc.html">BNCC</a></li>
-                                        <li><a href="co-curricolam/debate club.html">Debating Society</a></li>
-                                        <li><a href="co-curricolam/computer lab.html">Computer Lab</a></li>
-                                        <li><a href="co-curricolam/science lab.html">Science lab</a></li>
-                                    </ul>
-                            </li>
-                                <li><a href="faculty.html">Faculty</a></li>
-                                <li><a href="notice.html">Notice</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            APPLY NOW
-                          </button>
-
-                          <!-- Modal -->
-                          <div class="modal fade modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                              <div class="modal-content">
-                                <div class="modal-header">
-                                  <h1 class="modal-title fs-5" id="exampleModalLabel">Apply Now</h1>
-                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe0HzmSoscSLtcJtWcbGOdaVCEzeJqzbdsbhAJUX-D5RYp_A/viewform"><input type="radio">Degree</a><br>
-                                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe0HzmSoscSLtcJtWcbGOdaVCEzeJqzbdsbhAJUX-D5RYp_A/viewform"><input type="radio">Honour's</a><br>
-                                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe0HzmSoscSLtcJtWcbGOdaVCEzeJqzbdsbhAJUX-D5RYp_A/viewform"><input type="radio">Master's</a>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <?php include("php/include/navbar.php") ?>
 
         <!-- menu end -->
 
