64aa86923c2e214de5866370162b3396ad9b13f8
unknown <bushraaktermimno@gmail.com>
2025-01-19 15:36:28 +0600

    bncc page added

--- bncc.php
+++ bncc.php
@@ -1,16 +1,17 @@
 <html>
-<head><title>BNCC Page - BHRS College</title>
+<head>
+    <title>BNCC Page - BHRS College</title>
 
   <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <!-- font awesome -->
   <script src="https://kit.fontawesome.com/cdcfc18957.js" crossorigin="anonymous"></script>
   <!-- wow -->
-  <link rel="stylesheet" href="../css/animate.css">
+  <link rel="stylesheet" href="css/animate.css">
 
 
   <!-- wow -->
-  <script src="../js/wow.js"></script>
+  <script src="js/wow.js"></script>
   <script>
   new WOW().init();
   </script>
@@ -170,7 +171,7 @@
         
     .header2
         {
-        background-image:url(../img/co-curricolam-img/bncc/bnccbg.jpg);
+        background-image:url(img/co-curricolam-img/bncc/bnccbg.jpg);
        background-size: cover;
             height: 600px;
        }
@@ -288,73 +289,7 @@
 
     <!-- menu start -->
 
-    <section class="header">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-10">
-                <div class="wow bounceInUp logo logo" data-wow-duration="2s">
-                    <a href="../home.html"><img src="../img/logo/menu-logo.png" height="70px" width="70px"></a>
-                </div>
-                <div class="name">
-                    <strong><p><a href="../home.html">BHRS COLLEGE</a></p></strong>
-                </div>
-                  <div id="nav">
-                      <ul>
-                          <li class="padding"><a href="../home.html">Home</a></li>
-                          <li><a href="#">About Us &raquo;</a>
-                              <ul>
-                                  <li><a href="../about/breif-history.html">Brief History</a></li>
-                                  <li><a href="../about/gallery.html">Gallery</a></li>
-                              </ul>
-                          </li>
-                          <li><a href="#">Accademic &raquo;</a>
-                              <ul>
-                                  <li><a href="../accademic/Degree (Pass).html">DEGREE (PASS)</a></li>
-                                  <li><a href="../accademic/Honours.html">HONORS</a></li>
-                                  <li><a href="../accademic/master.html">MASTER'S</a></li>
-                              </ul>
-                          </li>
-                          <li><a href="#">Co-Curricolam &raquo;</a>
-                              <ul>
-                                  <li><a href="hostel info.html">Hostel Information</a></li>
-                                  <li><a href="librery info.html">Library Information</a></li>
-                                  <li><a href="bncc.html">BNCC</a></li>
-                                  <li><a href="debate club.html">Debating Society</a></li>
-                                  <li><a href="computer lab.html">Computer Lab</a></li>
-                                  <li><a href="science lab.html">Science lab</a></li>
-                              </ul>
-                      </li>
-                          <li><a href="../faculty.html">Faculty</a></li>
-                          <li><a href="../notice.html">Notice</a></li>
-                      </ul>
-                  </div>
-              </div>
-              <div class="col-md-2">
-                  <!-- Button trigger modal -->
-                  <button type="button" class="btn btn-primary button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                      APPLY NOW
-                    </button>
-
-                    <!-- Modal -->
-                    <div class="modal fade modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Apply Now</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                          </div>
-                          <div class="modal-body">
-                              <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe0HzmSoscSLtcJtWcbGOdaVCEzeJqzbdsbhAJUX-D5RYp_A/viewform"><input type="radio">Degree</a><br>
-                              <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe0HzmSoscSLtcJtWcbGOdaVCEzeJqzbdsbhAJUX-D5RYp_A/viewform"><input type="radio">Honour's</a><br>
-                              <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe0HzmSoscSLtcJtWcbGOdaVCEzeJqzbdsbhAJUX-D5RYp_A/viewform"><input type="radio">Master's</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-              </div>
-          </div>
-      </div>
-  </section>
+    <?php include("include/navbar.php") ?>
 
   <!-- menu end -->
 
@@ -549,7 +484,7 @@
 
            <!-- footer start -->
 
-           <?php include("../php/include/footer.php") ?>
+           <?php include("include/footer.php") ?>
   
   <!-- footer end -->
   </div>
