a6c566c8a13f739aac90c5b39e2186bb03445f9d
unknown <bushraaktermimno@gmail.com>
2025-01-19 15:52:04 +0600

    debate-club page added

--- debate-club.php
+++ debate-club.php
@@ -6,11 +6,11 @@
   <!-- font awesome -->
   <script src="https://kit.fontawesome.com/cdcfc18957.js" crossorigin="anonymous"></script>
   <!-- wow -->
-  <link rel="stylesheet" href="../css/animate.css">
+  <link rel="stylesheet" href="css/animate.css">
 
 
   <!-- wow -->
-  <script src="../js/wow.js"></script>
+  <script src="js/wow.js"></script>
   <script>
   new WOW().init();
   </script>
@@ -170,7 +170,7 @@
         
     .header2 
         {
-        background-image:url(../img/co-curricolam-img/debate/dbbg1.jpg);
+        background-image:url(img/co-curricolam-img/debate/dbbg1.jpg);
        background-size: cover;
             height: 600px;
        }
@@ -288,73 +288,7 @@
 
   <!-- menu start -->
 
-  <section class="header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10">
-                <div class="wow bounceInUp logo logo" data-wow-duration="2s">
-                    <a href="../home.html"><img src="../img/logo/menu-logo.png" height="70px" width="70px"></a>
-                </div>
-                <div class="name">
-                    <strong><p><a href="../home.html">BHRS COLLEGE</a></p></strong>
-                </div>
-                <div id="nav">
-                    <ul>
-                        <li class="padding"><a href="../home.html">Home</a></li>
-                        <li><a href="#">About Us &raquo;</a>
-                            <ul>
-                                <li><a href="../about/breif-history.html">Brief History</a></li>
-                                <li><a href="../about/gallery.html">Gallery</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Accademic &raquo;</a>
-                            <ul>
-                                <li><a href="../accademic/Degree (Pass).html">DEGREE (PASS)</a></li>
-                                <li><a href="../accademic/Honours.html">HONORS</a></li>
-                                <li><a href="../accademic/master.html">MASTER'S</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Co-Curricolam &raquo;</a>
-                            <ul>
-                                <li><a href="hostel info.html">Hostel Information</a></li>
-                                <li><a href="librery info.html">Library Information</a></li>
-                                <li><a href="bncc.html">BNCC</a></li>
-                                <li><a href="debate club.html">Debating Society</a></li>
-                                <li><a href="computer lab.html">Computer Lab</a></li>
-                                <li><a href="science lab.html">Science lab</a></li>
-                            </ul>
-                    </li>
-                        <li><a href="../faculty.html">Faculty</a></li>
-                        <li><a href="../notice.html">Notice</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-2">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    APPLY NOW
-                  </button>
-
-                  <!-- Modal -->
-                  <div class="modal fade modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h1 class="modal-title fs-5" id="exampleModalLabel">Apply Now</h1>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe0HzmSoscSLtcJtWcbGOdaVCEzeJqzbdsbhAJUX-D5RYp_A/viewform"><input type="radio">Degree</a><br>
-                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe0HzmSoscSLtcJtWcbGOdaVCEzeJqzbdsbhAJUX-D5RYp_A/viewform"><input type="radio">Honour's</a><br>
-                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe0HzmSoscSLtcJtWcbGOdaVCEzeJqzbdsbhAJUX-D5RYp_A/viewform"><input type="radio">Master's</a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-            </div>
-        </div>
-    </div>
-</section>
+  <?php include("include/navbar.php") ?>
 
 <!-- menu end -->
 
@@ -379,7 +313,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-    <img src="../img/co-curricolam-img/debate/abt.jpg" alt="img" class="img">
+    <img src="img/co-curricolam-img/debate/abt.jpg" alt="img" class="img">
       </div>
      </div>
            </div>
@@ -390,7 +324,7 @@
     <div class="row">
       
          <div class="col-md-6">
-    <img src="../img/co-curricolam-img/debate/db2.jpg" alt="img" class="img">
+    <img src="img/co-curricolam-img/debate/db2.jpg" alt="img" class="img">
           
            </div>
       </div>
@@ -438,7 +372,7 @@ MIST Bangladesh is administered by the Ministry of Defence (MoD). Bangladesh Uni
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-    <img src="../img/co-curricolam-img/debate/db4.jpg" alt="img" class="img">
+    <img src="img/co-curricolam-img/debate/db4.jpg" alt="img" class="img">
           
            </div>
       </div>
@@ -449,7 +383,7 @@ MIST Bangladesh is administered by the Ministry of Defence (MoD). Bangladesh Uni
   <div class="container">
     <div class="row">
          <div class="col-md-6">
-    <img src="../img/co-curricolam-img/debate/db5.jpg" alt="img" class="img">
+    <img src="img/co-curricolam-img/debate/db5.jpg" alt="img" class="img">
           
            </div>
       </div>
@@ -481,7 +415,7 @@ MIST Bangladesh is administered by the Ministry of Defence (MoD). Bangladesh Uni
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-    <img src="../img/co-curricolam-img/debate/db6.jpg" alt="img" class="img">
+    <img src="img/co-curricolam-img/debate/db6.jpg" alt="img" class="img">
           
            </div>
       </div>
@@ -492,7 +426,7 @@ MIST Bangladesh is administered by the Ministry of Defence (MoD). Bangladesh Uni
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-    <img src="../img/co-curricolam-img/debate/db7.jpg" alt="img" class="img">
+    <img src="img/co-curricolam-img/debate/db7.jpg" alt="img" class="img">
           
            </div>
       </div>
@@ -547,7 +481,7 @@ MIST Bangladesh is administered by the Ministry of Defence (MoD). Bangladesh Uni
 
          <!-- footer start -->
 
-         <?php include("../php/include/footer.php") ?>
+         <?php include("include/footer.php") ?>
   
   <!-- footer end -->
   </div>
